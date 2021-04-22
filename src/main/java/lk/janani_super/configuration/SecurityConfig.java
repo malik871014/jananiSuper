893de6f4ce0f50c23fddab20260b6bd5a60680f6
malik871014 <malik.koshala@outlook.com>
2021-04-22 14:37:01 +0530

    security configuration done for main window

--- src/main/java/lk/janani_super/configuration/SecurityConfig.java
+++ src/main/java/lk/janani_super/configuration/SecurityConfig.java
@@ -23,116 +23,118 @@ import org.springframework.security.web.session.HttpSessionEventPublisher;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-  private final String[] ALL_PERMIT_URL = {"/favicon.ico", "/img/**", "/css/**", "/js/**", "/webjars/**",
-      "/login", "/select/**", "/", "/index"};
+    private final String[] ALL_PERMIT_URL = {"/favicon.ico", "/img/**", "/css/**", "/js/**", "/webjars/**",
+            "/login", "/select/**", "/", "/index"};
 
-  @Bean
-  public UserDetailsServiceImpl userDetailsService() {
-    return new UserDetailsServiceImpl();
-  }
+    @Bean
+    public UserDetailsServiceImpl userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
 
-  @Bean
-  public BCryptPasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-  @Bean
-  public DaoAuthenticationProvider authenticationProvider() {
-    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-    authProvider.setUserDetailsService(userDetailsService());
-    authProvider.setPasswordEncoder(passwordEncoder());
-    return authProvider;
-  }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
 
-  /*Session management - bean start*/
-  @Bean
-  public HttpSessionEventPublisher httpSessionEventPublisher() {
-    return new HttpSessionEventPublisher();
-  }
+    /*Session management - bean start*/
+    @Bean
+    public HttpSessionEventPublisher httpSessionEventPublisher() {
+        return new HttpSessionEventPublisher();
+    }
 
-  @Bean
-  public SessionRegistry sessionRegistry() {
-    return new SessionRegistryImpl();
-  }
-  /*Session management - bean end*/
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+    /*Session management - bean end*/
 
-  @Bean
-  public AuthenticationSuccessHandler customAuthenticationSuccessHandler() {
-    return new CustomAuthenticationSuccessHandler();
-  }
+    @Bean
+    public AuthenticationSuccessHandler customAuthenticationSuccessHandler() {
+        return new CustomAuthenticationSuccessHandler();
+    }
 
-  @Bean
-  public LogoutSuccessHandler customLogoutSuccessHandler() {
-    return new CustomLogoutSuccessHandler();
-  }
+    @Bean
+    public LogoutSuccessHandler customLogoutSuccessHandler() {
+        return new CustomLogoutSuccessHandler();
+    }
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) {
-    auth.authenticationProvider(authenticationProvider());
-  }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
+    }
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
   /*  http.csrf().disable();
     http.authorizeRequests().antMatchers("/").permitAll();
 */
-    // For developing easy to give permission all lin
+        // For developing easy to give permission all lin
 // {"ADMIN","PROCUREMENT_MANAGER","CASHIER","MANAGER","HR_MANAGER","ACCOUNT_MANAGER"}
 
-    http.authorizeRequests(
-        authorizeRequests ->
-            authorizeRequests
-                .antMatchers(ALL_PERMIT_URL).permitAll()
-                .antMatchers("/category/**").hasAnyRole("ADMIN","PROCUREMENT_MANAGER","CASHIER","MANAGER")
-                .antMatchers("/category/**").hasAnyRole("CASHIER","MANAGER")
-                .antMatchers("/discountRatio/**").hasAnyRole("PROCUREMENT_MANAGER","MANAGER")
-                .antMatchers("/employee/**").hasAnyRole("MANAGER","HR_MANAGER" ,"ADMIN")
-                .antMatchers("/goodReceivedNote/**").hasAnyRole("MANAGER","PROCUREMENT_MANAGER")
-                .antMatchers("/payment/**").hasAnyRole("MANAGER","ACCOUNT_MANAGER")
-                .antMatchers("/purchaseOrder/**").hasAnyRole("MANAGER","PROCUREMENT_MANAGER")
-                .antMatchers("/role/**").hasAnyRole("MANAGER","HR_MANAGER","ADMIN")
-                .antMatchers("/supplier/**").hasAnyRole("MANAGER","PROCUREMENT_MANAGER")
-                .antMatchers("/supplierItem/**").hasAnyRole("MANAGER","PROCUREMENT_MANAGER")
-                .antMatchers("/user/**").hasAnyRole("MANAGER","HR_MANAGER","ADMIN")
-                .anyRequest()
-                .authenticated())
-        // Login form
-        .formLogin(
-            formLogin ->
-                formLogin
-                    .loginPage("/login")
-                    .loginProcessingUrl("/login")
-                    //Username and password for validation
-                    .usernameParameter("username")
-                    .passwordParameter("password")
-                    .successHandler(customAuthenticationSuccessHandler())
-                    .failureUrl("/login?error")
-                  )
-        //Logout controlling
-        .logout(
-            logout ->
-                logout
-                    .logoutUrl("/logout")
-                    .logoutSuccessHandler(customLogoutSuccessHandler())
-                    .deleteCookies("JSESSIONID")
-                    .invalidateHttpSession(true)
-                    .clearAuthentication(true))
-        //session management
-        .sessionManagement(
-            sessionManagement ->
-                sessionManagement
-                    .sessionFixation().migrateSession()
-                    .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
-                    .invalidSessionUrl("/login")
-                    .maximumSessions(6)
-                    .expiredUrl("/logout")
-                    .sessionRegistry(sessionRegistry()))
-        //Cross site disable
-        .csrf(AbstractHttpConfigurer::disable)
-        .exceptionHandling().and()
-        .headers()
-        .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
+        http.authorizeRequests(
+                authorizeRequests ->
+                        authorizeRequests
+                                .antMatchers(ALL_PERMIT_URL).permitAll()
+                                .antMatchers("/category/**").hasAnyRole("ADMIN", "PROCUREMENT_MANAGER", "CASHIER", "MANAGER")
+                                .antMatchers("/category/**").hasAnyRole("CASHIER", "MANAGER")
+                                .antMatchers("/discountRatio/**").hasAnyRole("PROCUREMENT_MANAGER", "MANAGER")
+                                .antMatchers("/employee/**").hasAnyRole("MANAGER", "HR_MANAGER", "ADMIN")
+                                .antMatchers("/goodReceivedNote/**").hasAnyRole("MANAGER", "PROCUREMENT_MANAGER")
+                                .antMatchers("/payment/**").hasAnyRole("MANAGER", "ACCOUNT_MANAGER")
+                                .antMatchers("/purchaseOrder/**").hasAnyRole("MANAGER", "PROCUREMENT_MANAGER")
+                                .antMatchers("/role/**").hasAnyRole("MANAGER", "HR_MANAGER", "ADMIN")
+                                .antMatchers("/supplier/**").hasAnyRole("MANAGER", "PROCUREMENT_MANAGER")
+                                .antMatchers("/supplierItem/**").hasAnyRole("MANAGER", "PROCUREMENT_MANAGER")
+                                .antMatchers("/user/**").hasAnyRole("MANAGER", "HR_MANAGER", "ADMIN")
+                                .antMatchers("/customer/**").hasAnyRole("CASHIER","MANAGER")
+                                .antMatchers("/item/**").hasAnyRole("CASHIER","MANAGER","PROCUREMENT_MANAGER")
+                                .anyRequest()
+                                .authenticated())
+                // Login form
+                .formLogin(
+                        formLogin ->
+                                formLogin
+                                        .loginPage("/login")
+                                        .loginProcessingUrl("/login")
+                                        //Username and password for validation
+                                        .usernameParameter("username")
+                                        .passwordParameter("password")
+                                        .successHandler(customAuthenticationSuccessHandler())
+                                        .failureUrl("/login?error")
+                )
+                //Logout controlling
+                .logout(
+                        logout ->
+                                logout
+                                        .logoutUrl("/logout")
+                                        .logoutSuccessHandler(customLogoutSuccessHandler())
+                                        .deleteCookies("JSESSIONID")
+                                        .invalidateHttpSession(true)
+                                        .clearAuthentication(true))
+                //session management
+                .sessionManagement(
+                        sessionManagement ->
+                                sessionManagement
+                                        .sessionFixation().migrateSession()
+                                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                                        .invalidSessionUrl("/login")
+                                        .maximumSessions(6)
+                                        .expiredUrl("/logout")
+                                        .sessionRegistry(sessionRegistry()))
+                //Cross site disable
+                .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling().and()
+                .headers()
+                .addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
 
-  }
+    }
 }
 
